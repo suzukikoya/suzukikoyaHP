2047be6c0b615ee2cb60b3185813d9e6a8820232
鈴木　航也 <kouyan9614@gmail.com>
2020-11-12 18:49:22 +0900

    スマホ対応_topページ_portfolio無駄な余白の削除

--- main.js
+++ main.js
@@ -34,3 +34,25 @@ $(function() {
 $('.menu-btn').on('click', function() {
   $('.menu').toggleClass('is-active');
 });
+
+function popupImage() {
+  var popup = document.getElementById('js-popup');
+  if(!popup) return;
+
+  var blackBg = document.getElementById('js-black-bg');
+
+  var blackBg = document.getElementById('js-black-bg');
+  var closeBtn = document.getElementById('js-close-btn');
+  var showBtn = document.getElementById('js-show-popup');
+
+  closePopUp(blackBg);
+  closePopUp(closeBtn);
+  closePopUp(showBtn);
+  function closePopUp(elem) {
+    if(!elem) return;
+    elem.addEventListener('click', function() {
+      popup.classList.toggle('is-show');
+    });
+  }
+}
+popupImage();
